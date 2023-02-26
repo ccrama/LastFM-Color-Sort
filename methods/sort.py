c0b85300653e47981d81c579c50af294da2e5b6c
ccrama <hapticappsdev@gmail.com>
2023-02-26 13:03:21 -0600

    Added horizontal and angled sort output

--- methods/sort.py
+++ methods/sort.py
@@ -12,25 +12,27 @@ def apply(image_mapping, output_file, angle=0):
     imindex = 0
 
 
-    while slice < m + n - 1:
-        z1 = 0 if slice < n else slice - n + 1
-        z2 = 0 if slice < m else slice - m + 1
-        j = j = slice - z2
-        while j >= z1:
-            im = Image.open(sorted_images[imindex])
-            im = im.resize((150, 150), Image.ANTIALIAS)
-            final_image.paste(im, (j * 150, (slice - j) * 150))
-            imindex += 1
-            j -= 1
-        slice += 1
-    
-
-    '''for i in range(0, max_edge_size):
-        for j in range(0, max_edge_size):
-            im = Image.open(sorted_images[imindex])
-            im = im.resize((150, 150), Image.ANTIALIAS)
-            final_image.paste(im, (i * 150, j * 150))
-            imindex += 1'''
+    # TODO make this work for any angle, currently its either 45 degrees or 0 degrees
+    if angle > 0:
+        while slice < m + n - 1:
+            z1 = 0 if slice < n else slice - n + 1
+            z2 = 0 if slice < m else slice - m + 1
+            
+            j = j = slice - z2
+            while j >= z1:
+                im = Image.open(sorted_images[imindex])
+                im = im.resize((150, 150), Image.ANTIALIAS)
+                final_image.paste(im, (j * 150, (slice - j) * 150))
+                imindex += 1
+                j -= 1
+            slice += 1
+    else :
+        for i in range(0, max_edge_size):
+            for j in range(0, max_edge_size):
+                im = Image.open(sorted_images[imindex])
+                im = im.resize((150, 150), Image.ANTIALIAS)
+                final_image.paste(im, (i * 150, j * 150))
+                imindex += 1
 
     # reduce image size
     final_image.save(output_file)
